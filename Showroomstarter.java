e6b8bc22d76b6e376fdcf017af08c923924e511c
reshmana <reshmanaik.xworkz@gmail.com>
2023-10-31 15:09:47 +0530

    constructor

---
+++ Showroomstarter.java
@@ -0,0 +1,113 @@
+class Showroomstarter {
+
+	public static void main(String[] args) {
+
+		Showroom showroom = new Showroom();
+
+		System.out.println("name of my showroom is : " + showroom.name);
+		System.out.println("type of my showroom is : " + showroom.type);
+		System.out.println("revenue of my showroom is : " + showroom.revenue);
+		System.out.println("ownername of my showroom is : " + showroom.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom.phoneno);
+		System.out.println("pin of my showroom is : " + showroom.pin);
+		System.out.println("adress of my showroom is : " + showroom.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom.Noofworkers);
+		System.out.println("***********");
+
+		Showroom showroom1 = new Showroom("yashas");
+		System.out.println("name of my showroom is : " + showroom1.name);
+		System.out.println("type of my showroom is : " + showroom1.type);
+		System.out.println("revenue of my showroom is : " + showroom1.revenue);
+		System.out.println("ownername of my showroom is : " + showroom1.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom1.phoneno);
+		System.out.println("pin of my showroom is : " + showroom1.pin);
+		System.out.println("adress of my showroom is : " + showroom1.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom1.Noofworkers);
+		System.out.println("***********");
+
+		Showroom showroom2 = new Showroom("yashas", "electric cycle");
+		System.out.println("name of my showroom is : " + showroom2.name);
+		System.out.println("type of my showroom is : " + showroom2.type);
+		System.out.println("revenue of my showroom is : " + showroom2.revenue);
+		System.out.println("ownername of my showroom is : " + showroom2.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom2.phoneno);
+		System.out.println("pin of my showroom is : " + showroom2.pin);
+		System.out.println("adress of my showroom is : " + showroom2.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom2.Noofworkers);
+		System.out.println("***********");
+
+		Showroom showroom3 = new Showroom("yashas", "rayal enfild", 10000000);
+		System.out.println("name of my showroom is : " + showroom3.name);
+		System.out.println("type of my showroom is : " + showroom3.type);
+		System.out.println("revenue of my showroom is : " + showroom3.revenue);
+		System.out.println("ownername of my showroom is : " + showroom3.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom3.phoneno);
+		System.out.println("pin of my showroom is : " + showroom3.pin);
+		System.out.println("adress of my showroom is : " + showroom3.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom3.Noofworkers);
+		System.out.println("***********");
+		Showroom showroom4 = new Showroom("yashas", "drone", 10000000, "yashodhar");
+		System.out.println("name of my showroom is : " + showroom4.name);
+		System.out.println("type of my showroom is : " + showroom4.type);
+		System.out.println("revenue of my showroom is : " + showroom4.revenue);
+		System.out.println("ownername of my showroom is : " + showroom4.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom4.phoneno);
+		System.out.println("pin of my showroom is : " + showroom4.pin);
+		System.out.println("adress of my showroom is : " + showroom4.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom4.Noofworkers);
+		System.out.println("***********");
+		Showroom showroom5 = new Showroom("yashas", "car", 100000000, "yashodhar", 9483397466l);
+		System.out.println("name of my showroom is : " + showroom5.name);
+		System.out.println("type of my showroom is : " + showroom5.type);
+		System.out.println("revenue of my showroom is : " + showroom5.revenue);
+		System.out.println("ownername of my showroom is : " + showroom5.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom5.phoneno);
+		System.out.println("pin of my showroom is : " + showroom5.pin);
+		System.out.println("adress of my showroom is : " + showroom5.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom5.Noofworkers);
+		System.out.println("***********");
+		Showroom showroom6 = new Showroom("yashas", "5star hotel", 100000000, "yashodhar", 9483397466l, 573103);
+		System.out.println("name of my showroom is : " + showroom6.name);
+		System.out.println("type of my showroom is : " + showroom6.type);
+		System.out.println("revenue of my showroom is : " + showroom6.revenue);
+		System.out.println("ownername of my showroom is : " + showroom6.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom6.phoneno);
+		System.out.println("pin of my showroom is : " + showroom6.pin);
+		System.out.println("adress of my showroom is : " + showroom6.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom6.Noofworkers);
+		System.out.println("***********");
+		Showroom showroom7 = new Showroom("yashas", "innovative studion", 100000000, "yashodhar", 9483397466l, 573103,
+				"near railway station arsikere");
+		System.out.println("name of my showroom is : " + showroom7.name);
+		System.out.println("type of my showroom is : " + showroom7.type);
+		System.out.println("revenue of my showroom is : " + showroom7.revenue);
+		System.out.println("ownername of my showroom is : " + showroom7.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom7.phoneno);
+		System.out.println("pin of my showroom is : " + showroom7.pin);
+		System.out.println("adress of my showroom is : " + showroom7.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom7.Noofworkers);
+		System.out.println("***********");
+		Showroom showroom8 = new Showroom("yashas", "international tour& travels", 100000000, "yashodhar", 9483397466l,
+				573103, "near railway station arsikere", 50);
+		System.out.println("name of my showroom is : " + showroom8.name);
+		System.out.println("type of my showroom is : " + showroom8.type);
+		System.out.println("revenue of my showroom is : " + showroom8.revenue);
+		System.out.println("ownername of my showroom is : " + showroom8.ownername);
+		System.out.println("phoneno of my showroom is : " + showroom8.phoneno);
+		System.out.println("pin of my showroom is : " + showroom8.pin);
+		System.out.println("adress of my showroom is : " + showroom8.adress);
+		System.out.println("Noofworkers in my showroom is : " + showroom8.Noofworkers);
+		System.out.println("***********");
+
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
