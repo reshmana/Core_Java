aecef66e595c1d5076f36898db3f4bb8ebc54eec
reshmana <reshmanaik.xworkz@gmail.com>
2023-11-03 10:59:25 +0530

    Constructor

---
+++ CourseStarter.java
@@ -0,0 +1,47 @@
+class CourseStarter{
+
+     public static void main(String[]args){
+	 
+	   System.out.println("starting of CourseStarter");
+	   
+	   Course course=new Course("X-workz","BTM",32000,"Corejava",5,10,"Exlent");
+	    course.displayInfo();
+		
+		Course course1=new Course("Jspider","BTM",50000,"Corejava",6,5,"good");
+	    course1.displayInfo();
+		
+		Course course2=new Course("Qspider","BTM",60000,"Corejava",5,15,"better");
+	    course2.displayInfo();
+		
+		Course course3=new Course("Lunix","jp nagr",4000,"python",5,4,"good");
+	    course3.displayInfo();
+		
+		Course course4=new Course("Adiksha","BTM",32000,"c++",5,10,"Exlent");
+	    course4.displayInfo();
+		
+		Course course5=new Course("Trinity info","BTM",52000,"Corejava",5,10,"Exlent");
+	    course5.displayInfo();
+		
+		Course course6=new Course("quicksteps","BTM",42000,"python",5,1,"Exlent");
+	    course6.displayInfo();
+		
+		Course course7=new Course("Skill tech","BTM",33000,"Corejava",5,10,"Exlent");
+	    course7.displayInfo();
+		
+		Course course8=new Course("Scon institute","marthalli",25000,"Corejava",5,4,"Exlent");
+	    course8.displayInfo();
+		
+		Course course9=new Course("coder academy","Banashankari",80000,"python",5,2,"Exlent");
+	    course9.displayInfo();
+		
+		Course course10=new Course("Digitech","yalahanka",450000,"Corejava",5,3,"Exlent");
+	    course10.displayInfo();
+		
+		Course course11=new Course("Zenixus","Malleshwarm",20000,"Corejava",5,8,"Exlent");
+	    course11.displayInfo();
+	   
+	   System.out.println("ending of CourseStarter");
+	 
+	 }
+
+}
