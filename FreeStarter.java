e096d1c0050e2d4be0fdeb8dbcb865aacb1d0450
reshmana <reshmanaik.xworkz@gmail.com>
2023-11-03 11:43:59 +0530

    Constructor

---
+++ FreeStarter.java
@@ -0,0 +1,50 @@
+class FreeStarter{
+ 
+    public static void main(String[]args){
+ 
+        System.out.println("Starting main in FreeStarter");
+		
+        Free free=new Free("pizza","LuckeyCoupun",false);
+         free.displayInfo();		
+		
+		Free free1=new Free("Car","LuckeyCoupun",false);
+          free1.displayInfo();		
+
+        Free free2=new Free("course","LuckeyCoupun",false);
+         free2.displayInfo();		
+
+        Free free3=new Free("Juice","LuckeyCoupun",false);
+         free3.displayInfo();		
+
+        Free free4=new Free("Icecream","LuckeyCoupun",false);
+         free4.displayInfo();
+
+         Free free5=new Free("wifi","to work",false);
+         free5.displayInfo();		
+          
+        Free free6=new Free("water","by Gov",true);
+         free6.displayInfo();		
+		  
+        Free free7=new Free("Air","To live",true);
+         free7.displayInfo();		
+         
+	    Free free8=new Free("Perents love ","we are there offspring",true);
+         free8.displayInfo();		
+         
+		Free free9=new Free("Bus ride for women","Gov Order",true);
+         free9.displayInfo();		
+
+        Free free10=new Free("rain","by nature",true);
+         free10.displayInfo();		
+
+        Free free11=new Free("sun light","by natur",true);
+         free11.displayInfo();		
+           
+		System.out.println("Starting main in FreeStarter");
+ 
+ 
+ }
+
+
+
+}
