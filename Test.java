961bb3c2a9dfd260b08945a0e8b76a5f9afcbf8d
reshmana <reshmanaik.xworkz@gmail.com>
2023-11-04 16:10:39 +0530

    Test 4/11

---
+++ Test.java
@@ -0,0 +1,141 @@
+class Test
+1.java is programming langauge, whic is used to develop the software.
+  javac is a compiler when the code end it compile the code 
+2.jdk[java development kite]
+
+  JRE.[java run time enviroment]
+   this is whole responsible for compile   the file.it consist of libraries.
+
+   JVM[java vertual machin]
+   it is used to conver the java code into byte code
+   byte code into java code(.class)
+ java virtual machin . it is whole responsible for exicute the fil.
+
+
+3.
+
+
+5. variable is memory location it is used to store the value or data , 
+      it can be change the No of time during exicution.
+     1. literals
+     2.methods
+     3.construtor
+     4.referance
+   1.literals are initilize the value to referance variavle 
+     example. String name ="Reshma"
+    Method is collection pf statement whic will get exicuted when it is called or invoked.
+   constructor. it is special type . it is used to initialize the variable
+   variable are 2 type 
+   class level
+   -->static variable
+   -->non static  variable/instance variable
+   method level
+   -->local variable
+    -->parameter
+   
+6. object oriented programming.
+   .incapsulation
+   .polymorphisum.
+   .inheritance
+   .abstraction.it is hiding the information
+
+8.data type:
+    the varies and size are stored wich representvie data tye
+     primitive data type
+    char
+   short
+   double
+   int
+   float
+   boolean 
+   byte
+  non primitive data type 
+   String
+   class 
+   arrey
+
+9.dot opperator use to access the variable name and method from the class
+    class 
+
+10. java features
+   . it is secure
+   .it is rebust
+   .object oriented
+   . multi- thread
+   . exicution and interprited
+   . it is a high speed
+
+
+11. arrey is collection of homogineus elements
+    arrey can be defined by using []
+      String [] arrey={1,2,3,4,5}
+
+12.class is a blue print to creat an object.
+       class is keyword
+      syntax{
+           
+            ////
+           }
+
+        class mobile{
+                   String brand; 
+                    int cost;
+                 }
+             
+    object: object is a real time intity.
+            it is instance of class
+
+
+13. keywords: keywords are pre-defined which are used in program
+         there are 48 keywords in java
+    new
+    super
+    this
+    absract
+    asset
+   final
+   boolean
+    byte
+    return
+    short
+    continue
+
+14.constructor: is a special method , it is used to initilize the varialbe 
+               construtor is a many copies.
+            we can write infinite constructor in one class
+             no return typeeturn
+            3 types of constructor
+            defalt constructor
+            parameter constructor
+            copy construtor
+              
+             
+15. main mathod is static becouse it assec the class name
+
+ 16 class arrey{
+        
+       public static void main(String[]args){
+       arrey[]={12,3,2,45,6,7,6};
+          for(int i=0; i<arrey.length;i++);
+          length arrey.[6]=7;
+           
+
+          System.out.printl("arrey length:"+[i]);
+}
+     }       
+               
+
+
+     
+
+
+
+
+
+
+     
+
+
+
+
+
